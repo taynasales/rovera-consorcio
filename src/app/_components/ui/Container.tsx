c08476ef8d34af8dbc8ac8cc5b289a6b082b77a4
taynasales <tayna.sales.dev@gmail.com>
2026-02-16 23:59:46 -0300

    feat: implementa Header com navegação

--- src/app/_components/ui/Container.tsx
+++ src/app/_components/ui/Container.tsx
@@ -4,29 +4,16 @@ import clsx from "clsx";
 interface ContainerProps {
   children: ReactNode;
   tag?: ElementType;
-  gutter?: "default" | "full";
   className?: string;
 }
-
-const gutters = {
-  default: "px-3 py-4",
-  full: "px-3 py-4",
-};
 
 export function Container({
   children,
   tag: Component = "section",
-  gutter = "default",
   className,
 }: ContainerProps) {
   return (
-    <Component
-      className={clsx(
-        "mx-auto w-full max-w-screen-xl",
-        gutters[gutter],
-        className,
-      )}
-    >
+    <Component className={clsx("mx-auto w-full max-w-screen-xl", className)}>
       {children}
     </Component>
   );
