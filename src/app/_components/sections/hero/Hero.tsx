78ffc201e70f2d8c02b62e36355cfa49dcb4e713
taynasales <tayna.sales.dev@gmail.com>
2026-02-18 11:29:02 -0300

    feat: adiciona imagem do Rovera X1

--- src/app/_components/sections/hero/Hero.tsx
+++ src/app/_components/sections/hero/Hero.tsx
@@ -1,12 +1,27 @@
+import Image from "next/image";
 import { Container } from "@/app/_components/ui/Container";
 import Title from "./Title";
 
 export default function Hero() {
   return (
-    <Container className="px-2 py-8 min-h-dvh xl:px-16">
+    <Container className="relative px-2 py-8 xl:px-16">
       <Title
         parts={["O seu", "futuro", "chegou"]}
         aria-label="O seu futuro chegou"
+      />
+      <Image
+        src="/images/hero/rovera-x1.png"
+        alt="Rovera X1, carro elétrico em destaque"
+        width={620}
+        height={440}
+        priority
+        className="
+        block mx-auto
+        xl:absolute xl:left-1/2 xl:top-1/2 xl:-translate-x-1/2
+        xl:-translate-y-[calc(0%+5.6rem)]
+        xl:-ml-3
+        z-10
+        "
       />
     </Container>
   );
